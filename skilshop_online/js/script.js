1cabadd02441bb3f732a3f1eb9ac8a332718975e
Sasindu Nethsara Karanayaka <110768660+Nethsara14@users.noreply.github.com>
2026-02-12 19:17:04 +0530

    Initial upload of Skill Buy&Sell project files

---
+++ skilshop_online/js/script.js
@@ -0,0 +1,24 @@
+function toggleForms() {
+
+    var signinForm = document.getElementById("signin-form");
+    var signupForm = document.getElementById("signup-form");
+
+    signinForm.classList.toggle("hidden");
+    signinForm.classList.toggle("active");
+
+    signupForm.classList.toggle("hidden");
+    signupForm.classList.toggle("active");
+
+}    
+
+
+function togglePassword(inputId, btn) {
+     var input = document.getElementById(inputId);
+     if (input.type == "password") {
+         input.type = "text";
+         btn.innerHTML = "Hide";
+     } else {
+         input.type = "password";
+         btn.innerHTML = "Show";
+     }
+}
