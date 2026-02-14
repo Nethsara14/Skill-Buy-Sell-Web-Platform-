22bed8031cfd263e75344e97aba668c6bec28bbb
Sasindu Nethsara Karanayaka <110768660+Nethsara14@users.noreply.github.com>
2026-02-14 17:53:18 +0530

    Add createAccount function for user registration

---
+++ skilshop_online/js/outh.js
@@ -0,0 +1,63 @@
+function createAccount() {
+    var fname = document.getElementById("signup-firstname").value;
+    var lname = document.getElementById("signup-lastname").value;
+    var email = document.getElementById("signup-email").value;
+    var password = document.getElementById("signup-password").value;
+    var cpassword = document.getElementById("signup-confirm").value;
+    var accountTypeSeller = document.getElementById("account_type_seller");
+    var accountTypeBuyer = document.getElementById("account_type_buyer");
+    var termsConditions = document.getElementById("terms_conditions");
+    var signupErrorMessage = document.getElementById('signup-message');
+
+    signupErrorMessage.classList.remove("hidden");
+
+    if (!fname || !lname || !email || !password || !cpassword) {
+        signupErrorMessage.innerHTML = "All fields are required";
+    } else if (password.length < 8) {
+        signupErrorMessage.innerHTML = "Password must be at least 8 characters";
+    } else if (password != cpassword) {
+        signupErrorMessage.innerHTML = "Passwords do not match";
+    } else if (!accountTypeSeller.checked && !accountTypeBuyer.checked) {
+        signupErrorMessage.innerHTML = "Please select account type";
+    } else if (!termsConditions.checked) {
+        signupErrorMessage.innerHTML = "Please read and check I agree to the Terms & Conditions";
+    } else {
+        signupErrorMessage.classList.add("hidden");
+
+        var form = new FormData();
+        form.append("fname", fname);
+        form.append("lname", lname);
+        form.append("email", email);
+        form.append("password", password);
+        form.append("pass_confirm", cpassword);
+        form.append("account_type", accountTypeSeller.checked ? accountTypeSeller.value : accountTypeBuyer.value);
+        form.append("terms_conditions", termsConditions.checked);
+
+        var r = new XMLHttpRequest();
+
+        r.onreadystatechange = function () {
+            if (r.readyState == 4) {
+                signupErrorMessage.classList.remove("hidden");
+
+                if (r.status == 200) {
+                    if (r.responseText == "success") {
+                        signupErrorMessage.classList.remove("text-red-500");
+                        signupErrorMessage.classList.add("text-green-500");
+                        signupErrorMessage.innerHTML = "Registration Successfull!";
+                        setTimeout(() => {
+                            window.location.reload();
+                        }, 2000);
+                    } else {
+                        signupErrorMessage.innerHTML = r.responseText;
+                    }
+                } else {
+                    signupErrorMessage.innerHTML = "Request failed! : " + r.responseText;
+                }
+            }
+        };
+
+        r.open("POST", "process/createAccountProcess.php", true);
+        r.send(form);
+    }
+    
+}
